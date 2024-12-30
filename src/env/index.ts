e34af9ededf65459f13e652c558f38a7c9b1c835
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-12-30 11:34:56 -0400

    Valida variáveis de ambiente com Zod
    - Adiciona schema de validação para `DATABASE_URL`, `SECRET_KEY_JWT` e `SECRET_KEY_PASSWORD`.
    - Implementa validação no carregamento das variáveis usando `z.safeParse`.
    - Gera erro e encerra o processo caso as variáveis de ambiente sejam inválidas.

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,7 +1,16 @@
-import z from 'zod'
+import { z } from 'zod'
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
+  SECRET_KEY_JWT: z.string(),
+  SECRET_KEY_PASSWORD: z.string(),
 })
 
-export const env = envSchema.parse(process.env)
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  console.error('❌ Invalid environment variables:', _env.error.format())
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
